f1f1bb53f7c3fc9ff4ce3c379576601ee4061c11
Sehyeon Youn <dev.younlab@gmail.com>
2019-05-27 17:52:22 +0900

    [UPDATE] node email update

---
+++ src/utils.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+import { adjectives, nouns } from './words';
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
+
+export const secretGenerator = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD,
+    },
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: 'news.younlab@gmail.com',
+    to: adress,
+    subject: 'Login Secret for Prismagram',
+    html: `Hello! Your Login Secret it ${secret}.<br/>Copy paste on the app to login`,
+  };
+  return sendMail(email);
+};
